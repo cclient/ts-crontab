0aac0e4f8b19207a2895d76e72f274bcb775c8f6
Cui dapeng <cclient@hotmail.com>
2017-12-06 15:28:48 +0800

    fix elasticsearch-js/issues/33
    https://github.com/elasticsearch/elasticsearch-js/issues/33

--- src/utils/es.ts
+++ src/utils/es.ts
@@ -2,13 +2,20 @@
 
 import * as elasticsearch from "elasticsearch";
 
+let client=null;
+
 export function getESConnection() {
-    let client = new elasticsearch.Client({
+    if(client != null){
+        return client
+    }else{
+         client = new elasticsearch.Client({
         // host: eshost + ":9200",
-        // log: 'trace'
-    });
-    return client
+        // log: 'trace'             
+         });
+         return client
+    }
 }
+
 
 
 
